24d1e487447722fb93b91a8a483a4b7a00914a83
mohammad-tawhid <mohammad.tawhid@bkash.com>
2023-02-27 12:54:29 +0600

    credential-remove

--- variable.tf
+++ variable.tf
@@ -1,10 +1,10 @@
 variable "aws_access_key" {
   description = "aws_access_key"
   type        = string
-  default     = "AKIAWNO5LQORTT4PHJVI"
+  default     = ""
 }
 variable "aws_secret_key" {
   description = "aws_secret_key"
   type        = string
-  default     = "iy8gck1uOg9s29qy+G5dsGZaBth9Af+3Q3R+rIUr"
+  default     = ""
 }
