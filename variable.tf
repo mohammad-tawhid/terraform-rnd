a0f325c0518c720a59ea79967e1973a09631ddb7
mohammad-tawhid <mohammad.tawhid@bkash.com>
2023-02-27 12:12:47 +0600

    aws-info-added

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "aws_access_key" {
+  description = "aws_access_key"
+  type        = string
+  default     = "AKIAWNO5LQORTT4PHJVI"
+}
+variable "aws_secret_key" {
+  description = "aws_secret_key"
+  type        = string
+  default     = "iy8gck1uOg9s29qy+G5dsGZaBth9Af+3Q3R+rIUr"
+}
