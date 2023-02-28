82684f998995c8188c8e792721186ba88ddd2043
mohammad-tawhid <mohammad.tawhid@bkash.com>
2023-02-28 13:45:41 +0600

    22 add

--- main.tf
+++ main.tf
@@ -103,7 +103,14 @@ resource "aws_security_group" "terraform-ssh-access" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
 	}
-
+	
+	ingress {
+	from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+	}
+	
 	egress {
 	from_port = 0
 	to_port = 0
